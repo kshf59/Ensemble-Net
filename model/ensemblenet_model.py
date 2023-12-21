3733b7d1abc564f8b15ae1beb7382be5c1eff1c3
DataScientist <kshf59@naver.com>
2023-12-21 01:43:17 -0500

    code_fix_ensemblenet

--- model/ensemblenet_model.py
+++ model/ensemblenet_model.py
@@ -2,11 +2,44 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from model.unet.unet_model import UNet
-from model.segnet.segnet_model import SegNet
+#from model.unet.unet_model import UNet
+#from model.segnet.segnet_model import SegNet
 
 
 
+
+class EnsembleNet(nn.Module):
+    def __init__(self, model_unet, model_segnet):
+        super(EnsembleNet, self).__init__()
+        
+        
+        self.unet = model_unet
+        self.segnet = model_segnet
+        self.n_classes = model_unet.n_classes
+        self.n_channels = model_unet.n_channels
+        
+        self.conv_layer1 = nn.Conv2d(self.n_classes, self.n_classes, kernel_size=1)
+        self.conv_layer2 = nn.Conv2d(self.n_classes, self.n_classes, kernel_size=1)
+        
+        
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self, x):
+        
+        unet_out = self.unet(x)
+        segnet_out = self.segnet(x)
+        
+        
+        out = torch.mul(unet_out, segnet_out)
+        out = self.conv_layer1(out)
+        #out = self.relu(out)
+
+        return out
+
+
+
+
+'''
 class EnsembleNet(nn.Module):
     def __init__(self, n_channels, n_classes):
         super(EnsembleNet, self).__init__()
@@ -17,28 +50,36 @@ class EnsembleNet(nn.Module):
         self.unet = UNet(n_channels=self.n_channels, n_classes=self.n_classes, bilinear=True)
         self.segnet = SegNet(n_channels=self.n_channels, n_classes=self.n_classes)
         
-        #self.conv1 = nn.Conv2d(self.n_ch, self.n_cl, kernel_size=3, padding=1)
+        self.conv_layer = nn.Conv2d(64, self.n_classes, kernel_size=3, padding=1)
         #self.fusion_layer = self.fusion(self.unet, self.segnet)
-    '''
+        
+        
+    
     def voting(self, x):
         pass
         
     def stacking(self, x):
         pass
-    '''
     
     def fusion(self, model1, model2):
         
-        return model1, model2
+        result = torch.mul(model1, model2)
         
+        return result
+    
+    
     def forward(self, x):
         
         unet = self.unet(x)
-        segnet = self.segnet(x)
-        #out1, out2 = self.fusion(unet, segnet)
-        #return result1, result2
-        return unet, segnet
-    
+        #segnet = self.segnet(x)
+        
+        
+        #out = torch.mul(unet, segnet)
+        
+        #out = self.conv_layer(fusion)
+
+        return unet
+'''
 
 
 
